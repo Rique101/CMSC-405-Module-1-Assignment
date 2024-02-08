46576b9a370d2b48f57d1596194e3f37f0e9608f
Enrique Tejada <Etejada2347@gmail.com>
2024-02-08 10:58:48 -0500

    Module 1 Assignment Programming Languages Spring 2024

---
+++ App.c
@@ -0,0 +1,9 @@
+// pre-processor directives
+
+// include needed C and header files
+#include "Assignment.c"
+// write code for main function
+
+int main(void){
+    printf("Intensity    Target Heart Rate");
+}
---
+++ Assignment.c
@@ -0,0 +1,23 @@
+// pre-processor directives
+
+// include needed header file
+#include "./headers/Assignment.h"
+
+// write code for functions declared in header file
+
+void heartRate(int, int){
+    float targetHeartRate = (((220 - age) - restingPulse) * 0.50) + restingPulse
+
+    // declare and initialize a counter variable
+    // for our loop
+    int i = START;
+
+    // set up while loop using macro expression
+    // as its condition
+    while(START(i)){
+        printf("%d ", i);
+        i++;
+    }
+
+    return targetHeartRate;
+}
